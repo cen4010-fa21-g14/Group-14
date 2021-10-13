36db3c7d0beb5d40667faf9b0915cb6c726f4c56
ErnestoLz <ernestolopez2018@fau.edu>
2021-10-13 17:30:59 -0400

    multiple working icons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import Home from "./pages/home/Home";
 
 import MenuIcon from '@mui/icons-material/Menu';
-import PersonIcon from '@mui/icons-material/Person';
+import {Person, Agriculture } from '@mui/icons-material';
 
 function App() {
-  return <PersonIcon/>
+  return (
+    <>
+  <Home/>
+  <Person/>
+  <MenuIcon/>
+  <Agriculture/>
+    </>
+  )
 }
 
 export default App;
