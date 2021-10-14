47a3c77247c646e03a3ebfa6b2b61a9c728e7c67
andresbotia <abotia2018@fau.edu>
2021-10-14 19:38:25 -0400

    updating files and adding Online Component

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,33 +1,35 @@
 import "./post.css"
 import {MoreVert} from '@mui/icons-material';
+import {Users} from "../..//dummyData"
 
-export default function post() {
+export default function post({post}) {
+
     return (
         <div className="post">
             <div className="postWrapper">
                 <div className="postTop">
                     <div className="postTopLeft">
-                        <img className="postProfileImg" alt="" src="/assets/person/4.jpg"></img>
-                        <span className="postUsername">John Doe</span>
-                        <span className="postDate">5 mins ago</span>
+                        <img className="postProfileImg" alt="" src={Users.filter(u=>u.id === post.userId)[0].profilePicture}></img>
+                        <span className="postUsername">{Users.filter(u=>u.id === post.userId)[0].username}</span>
+                        <span className="postDate">{post.date}</span>
                     </div>
                     <div className="postTopRight">
                         <MoreVert/>
                     </div>
                 </div>
                 <div className="postCenter">
-                    <span className="postText">Hey! Its my first post :)</span>
-                    <img className="postImg" src="/assets/post/1.jpg" alt=""></img>
+                    <span className="postText">{post?.desc}</span>
+                    <img className="postImg" src={post.photo} alt=""></img>
                 </div>
                 <div className="postBottom">
                     <div className="postBottomLeft">
                         <img className="likeIcon" alt="" src="/assets/like.png"></img>
                         <img className="likeIcon" alt="" src="/assets/heart.png"></img>
-                        <span className="postBottomRight">
-                            <span className="postCommentText">9 Comments</span>
-                        </span>
+                        <span className="postLikeCounter">{post.like} people like it </span>
                     </div>
-                    <div className="postBottomRight"></div>
+                    <div className="postBottomRight">
+                        <span className="postCommentText">  {post.comment} Commented</span>
+                    </div>
                 </div>
             </div>
         </div>
--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -1,4 +1,6 @@
 import "./rightbar.css" 
+import {Users} from "../../dummyData"
+import Online from "../online/Online";
 
 export default function Rightbar() {
     return (
@@ -6,19 +8,18 @@ export default function Rightbar() {
             <div className="rightbarWrapper">
                 <div className="birthdayContainer">
                     <img src="assets/birthday.png" alt="" className="birthdayImg"></img>
-                    <span className="birthdayText"> <b>Pola Foster</b> and <b>3 other friends</b> have a birthday today</span>
+                    <span className="birthdayText"> 
+                        <b>Pola Foster</b> and <b>3 other friends</b> have a birthday today.
+                    </span>
                 </div>
+                <img className="rightbarAd" src="assets/ad.png" alt=""></img>
             </div>
-            <img className="rightbarAd" src="assets/ad.png" alt=""></img>
+            
             <h4 className="rightBarTitle">Online Friends</h4>
             <ul className="rightbarFriendList">
-                <li className="rightbarFriend">
-                    <div className="rightbarProfileImgContainer">
-                        {/* <img alt="rightbarProfileImg" src="assets/person/2.jpg"></img>   */}
-                        <span className="rightbarOnline"></span> 
-                    </div>
-                    <span className="rightbarUsername">John Doe</span>
-                </li>
+               {Users.map(u=>(
+                   <Online key={u.id} user={u}/>
+               ))}
             </ul>
         </div>
     )
